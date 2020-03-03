3e447dd8a8c06bab02f1a819ae1796e4e58a30c4
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-03-03 12:45:28 +0900

    既存コンポーネントをTypeScriptで書き直し、リント修正

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,58 +1,68 @@
 <template>
-    <div class="card">
-        <transition>
-            <div v-if="!card.turned" class="content" key="back">
-                <div class="content back" @click="$emit('turn')"></div>
-            </div>
-            <div v-else class="content" key="front">
-                <img class="content" :src="imagePath" alt="cat" />
-            </div>
-        </transition>
-    </div>
+  <div class="card">
+    <transition>
+      <div v-if="!card.turned" class="content" key="back">
+        <div class="content back" @click="$emit('turn')"></div>
+      </div>
+      <div v-else class="content" key="front">
+        <img class="content" :src="imagePath" alt="cat" />
+      </div>
+    </transition>
+  </div>
 </template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import CardInfo from "@/types/CardInfo";
+
+@Component({
+  name: "Card"
+})
+export default class Card extends Vue {
+  @Prop() card!: CardInfo;
+
+  get imagePath() {
+    return require(`@/assets/cat-${this.card.number}.png`);
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 @mixin size() {
-    width: 100px;
-    height: 175px;
+  width: 100px;
+  height: 175px;
 }
 .card {
-    @include size();
-    position: relative;
+  @include size();
+  position: relative;
 }
 .content {
-    @include size();
-    margin: 0;
-    border-radius: 5px;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
-    position: absolute;
-    top: 0;
-    left: 0;
-    transition: transform 0.1s, opacity 0.1s;
+  @include size();
+  margin: 0;
+  border-radius: 5px;
+  box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
+  position: absolute;
+  top: 0;
+  left: 0;
+  transition: transform 0.1s, opacity 0.1s;
 }
 .back {
-    background: repeating-linear-gradient(90deg, #F00, #F00 5px, #F66 0, #F66 6px);
+  background: repeating-linear-gradient(
+    90deg,
+    #f00,
+    #f00 5px,
+    #f66 0,
+    #f66 6px
+  );
 }
-.v-enter, .v-leave-to {
-    transform: rotateY(0deg);
-    opacity: 0;
+.v-enter,
+.v-leave-to {
+  transform: rotateY(0deg);
+  opacity: 0;
 }
-.v-enter-to, .v-leave {
-    transform: rotateY(90deg);
-    opacity: 1;
+.v-enter-to,
+.v-leave {
+  transform: rotateY(90deg);
+  opacity: 1;
 }
 </style>
-
-<script>
-export default {
-    name: 'Card',
-    props: {
-        card: Object
-    },
-    computed: {
-        imagePath: function() {
-            return require(`@/assets/cat-${this.card.number}.png`);
-        }
-    }
-}
-</script>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,113 +1,121 @@
 <template>
-    <main class="wrapper">
-        <section class="scoreArea">
-            <div class="centeringContainer">
-                <p>TRY: {{ trial_count | zero_padding }}</p>
-                <p>SCORE: {{ matched_pair_count | zero_padding }}</p>
-            </div>
-        </section>
+  <main class="wrapper">
+    <section class="scoreArea">
+      <div class="centeringContainer">
+        <p>TRY: {{ trialCount | zeroPadding }}</p>
+        <p>SCORE: {{ matchedPairCount | zeroPadding }}</p>
+      </div>
+    </section>
 
-        <div class="centeringContainer">
-            <section class="cardTable">
-                <Card v-for="(card, index) in cards"
-                    :card="card" :key="index" @turn="select_card(index)" />
-            </section>
-        </div>
-    </main>
+    <div class="centeringContainer">
+      <section class="cardTable">
+        <Card
+          v-for="(card, index) in cards"
+          :card="card"
+          :key="index"
+          @turn="selectCard(index)"
+        />
+      </section>
+    </div>
+  </main>
 </template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Card from "@/components/Card.vue";
+import CardInfo from "@/types/CardInfo";
+
+const CARD_PAIR_SET = 10;
+
+@Component({
+  name: "Table",
+  filters: {
+    zeroPadding(value: number) {
+      return ("00" + String(value)).slice(-3);
+    }
+  },
+  components: {
+    Card
+  }
+})
+export default class Table extends Vue {
+  cards: CardInfo[] = [];
+  selectedCardIndexes: number[] = [];
+  trialCount = 0;
+  matchedPairCount = 0;
+
+  prepareCards() {
+    for (let i = 0; i < CARD_PAIR_SET; i++) {
+      const card = {
+        number: i,
+        turned: false,
+        matched: false
+      };
+      // 対になる様、各数字2つずつ用意
+      this.cards.push(card, Object.assign({}, card));
+    }
+  }
+
+  shuffleCards() {
+    this.cards.sort(() => Math.random() - Math.random());
+  }
+
+  selectCard(index: number): void {
+    if (this.selectedCardIndexes.length > 1) return;
+
+    this.selectedCardIndexes.push(index);
+    this.cards[index].turned = true;
+
+    if (this.selectedCardIndexes.length === 2) {
+      window.setTimeout(this.checkPair, 1000);
+    }
+  }
+
+  checkPair(): void {
+    const firstCard = this.cards[this.selectedCardIndexes[0]];
+    const secondCard = this.cards[this.selectedCardIndexes[1]];
+    this.trialCount++;
+
+    if (firstCard.number === secondCard.number) {
+      this.fixPair();
+    } else {
+      this.closePair();
+    }
+  }
+
+  fixPair(): void {
+    this.selectedCardIndexes.map(index => (this.cards[index].matched = true));
+    this.selectedCardIndexes = [];
+    this.matchedPairCount++;
+  }
+
+  closePair(): void {
+    this.selectedCardIndexes.map(index => (this.cards[index].turned = false));
+    this.selectedCardIndexes = [];
+  }
+
+  created(): void {
+    this.prepareCards();
+    this.shuffleCards();
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .scoreArea {
-    background-color: #444;
-    color: white;
-    font-family: 'Press Start 2P', cursive;
-    line-height: 1.8;
-    text-align: right;
-    padding: 15px 0;
-    margin-bottom: 60px;
+  background-color: #444;
+  color: white;
+  font-family: "Press Start 2P", cursive;
+  line-height: 1.8;
+  text-align: right;
+  padding: 15px 0;
+  margin-bottom: 60px;
 }
 .cardTable {
-    display: grid;
-    grid-template-columns: repeat(10, 100px);
-    grid-template-rows: repeat(2, 175px);
-    column-gap: 15px;
-    row-gap: 15px;
+  display: grid;
+  grid-template-columns: repeat(10, 100px);
+  grid-template-rows: repeat(2, 175px);
+  column-gap: 15px;
+  row-gap: 15px;
 }
-
 </style>
-
-<script>
-import Card from './Card.vue'
-
-const CARD_PAIR_SET = 10
-
-export default {
-    name: 'Table',
-    components: {
-        Card
-    },
-    data: function() {
-        return {
-            cards: [],
-            selected_card_indexes: [],
-            trial_count: 0,
-            matched_pair_count: 0
-        }
-    },
-    methods: {
-        prepare_cards: function() {
-            for (let i=0; i<CARD_PAIR_SET; i++) {
-                const card = {
-                    number: i,
-                    turned: false,
-                    matched: false
-                }
-                // 対になる様、各数字2つずつ用意
-                this.cards.push(card, Object.assign({}, card))
-            }
-        },
-        shuffle_cards: function() {
-            this.cards.sort(() => Math.random() - Math.random())
-        },
-        select_card: function(index) {
-            if (this.selected_card_indexes.length > 1) return
-
-            this.selected_card_indexes.push(index)
-            this.cards[index].turned = true
-
-            if (this.selected_card_indexes.length === 2) {
-                window.setTimeout(this.check_pair, 1000)
-            }
-        },
-        check_pair: function() {
-            const first_card = this.cards[this.selected_card_indexes[0]]
-            const second_card = this.cards[this.selected_card_indexes[1]]
-            this.trial_count ++
-
-            if (first_card.number === second_card.number) {
-                this.fix_pair()
-            } else {
-                this.close_pair()
-            }
-        },
-        fix_pair: function() {
-            this.selected_card_indexes.map(index => this.cards[index].matched = true)
-            this.selected_card_indexes = []
-            this.matched_pair_count ++
-        },
-        close_pair: function() {
-            this.selected_card_indexes.map(index => this.cards[index].turned = false)
-            this.selected_card_indexes = []
-        }
-    },
-    filters: {
-        zero_padding: function(value) {
-            return ('00' + value).slice(-3)
-        }
-    },
-    created: function() {
-        this.prepare_cards()
-        this.shuffle_cards()
-    }
-}
-</script>
