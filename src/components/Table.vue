6d3de96df0258cf85acd797b897e79fd96d108d5
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2020-03-07 02:49:15 +0900

    Vuexを使った形に修正

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -2,18 +2,18 @@
   <main class="wrapper">
     <section class="scoreArea">
       <div class="centeringContainer">
-        <p>TRY: {{ trialCount | zeroPadding }}</p>
-        <p>SCORE: {{ matchedPairCount | zeroPadding }}</p>
+        <p>TRY: {{ trialCount }}</p>
+        <p>SCORE: {{ matchedPairCount }}</p>
       </div>
     </section>
 
     <div class="centeringContainer">
       <section class="cardTable">
         <Card
-          v-for="(card, index) in cards"
+          v-for="(card, index) in cardList"
           :card="card"
           :key="index"
-          @turn="selectCard(index)"
+          @turn="onCardTurned(index)"
         />
       </section>
     </div>
@@ -23,80 +23,31 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Card from "@/components/Card.vue";
-import CardInfo from "@/types/CardInfo";
-
-const CARD_PAIR_SET = 10;
+import CardObject from "@/models/Card";
+import CardList from "@/store/modules/card-list";
+import GameInfo from "@/store/modules/game-info";
 
 @Component({
   name: "Table",
-  filters: {
-    zeroPadding(value: number) {
-      return ("00" + String(value)).slice(-3);
-    }
-  },
   components: {
     Card
   }
 })
 export default class Table extends Vue {
-  cards: CardInfo[] = [];
-  selectedCardIndexes: number[] = [];
-  trialCount = 0;
-  matchedPairCount = 0;
-
-  prepareCards() {
-    for (let i = 0; i < CARD_PAIR_SET; i++) {
-      const card = {
-        number: i,
-        turned: false,
-        matched: false
-      };
-      // 対になる様、各数字2つずつ用意
-      this.cards.push(card, Object.assign({}, card));
-    }
+  get cardList(): CardObject[] {
+    return CardList.cardList;
   }
 
-  shuffleCards() {
-    this.cards.sort(() => Math.random() - Math.random());
+  get trialCount(): string {
+    return GameInfo.trialCount;
   }
 
-  selectCard(index: number): void {
-    if (this.selectedCardIndexes.length > 1) return;
-
-    this.selectedCardIndexes.push(index);
-    this.cards[index].turned = true;
-
-    if (this.selectedCardIndexes.length === 2) {
-      window.setTimeout(this.checkPair, 1000);
-    }
+  get matchedPairCount(): string {
+    return GameInfo.matchedPairCount;
   }
 
-  checkPair(): void {
-    const firstCard = this.cards[this.selectedCardIndexes[0]];
-    const secondCard = this.cards[this.selectedCardIndexes[1]];
-    this.trialCount++;
-
-    if (firstCard.number === secondCard.number) {
-      this.fixPair();
-    } else {
-      this.closePair();
-    }
-  }
-
-  fixPair(): void {
-    this.selectedCardIndexes.map(index => (this.cards[index].matched = true));
-    this.selectedCardIndexes = [];
-    this.matchedPairCount++;
-  }
-
-  closePair(): void {
-    this.selectedCardIndexes.map(index => (this.cards[index].turned = false));
-    this.selectedCardIndexes = [];
-  }
-
-  created(): void {
-    this.prepareCards();
-    this.shuffleCards();
+  onCardTurned(index: number): void {
+    CardList.selectCard(index);
   }
 }
 </script>
